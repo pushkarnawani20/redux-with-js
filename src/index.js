029d78b115eb6e9d9e1c5d806f39bbf9a9a0f7c0
pushkar <pushkarnawani20@gmail.com>
2018-11-20 20:35:16 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore,combineReducers,applyMiddleware} from 'redux';// import store, combineReducers,applyMiddleware from redux
+import logger from "redux-logger";//import logger middleware from redux-logger
+// set initial state
+const initialSate = {
+  initialValue:0,
+  listArray:[]
+}
+// first reducer
+const mathReducer = (state = initialSate,action) => {
+  switch (action.type) {
+    case "ADD":
+      state = {
+        ...state,
+        initialValue:state.initialValue + action.payload,
+        listArray:[...state.listArray, action.payload]
+      };
+      break
+    case "SUBTRACT":
+    state = {
+      ...state,
+      initialValue:state.initialValue - action.payload,
+      listArray:[...state.listArray, action.payload]
+    };
+    break;
+  }
+  return state;
+}
+// second reducer
+const addTodoReducer = (state = {todoList:[]},action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      state = {
+        ...state,
+        todoList:[...state.todoList, action.payload]
+      };
+      break
+  }
+  return state;
+}
+// createStore(reducers,initialSate,middleware)
+// create store with single reducer
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ // let store = createStore(mathReducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ // create store with multiple reducer using "combineReducers" combineReducers({js object})
+
+// let store = createStore(combineReducers({
+//   mathStore:mathReducer,
+//   todoList:addTodoReducer
+// }));
+
+// create store with middleware "applyMiddleware"
+
+// create a custom middleware
+const myLogger = (store) => (next) => (action) =>{
+  console.log("custom Logger action",action);
+  next(action);
+}
+
+let store = createStore(combineReducers({
+  mathStore:mathReducer,
+  todoList:addTodoReducer
+}),{},applyMiddleware(logger, myLogger));
+
+window.store = store;
+// store.subscribe(() =>{
+//   console.log("store data",store.getState());
+// })
+// store.dispatch({// dispatch any action
+//   type:"ADD",
+//   payload:10
+// });
